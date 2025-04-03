c59075f585b520fb6001861f98b10ac0d400e7d9
Cyang8980 <78817877+Cyang8980@users.noreply.github.com>
2025-04-04 07:43:56 +0800

    made changes to profile

--- client/pages/profile/[[...index]].tsx
+++ client/pages/profile/[[...index]].tsx
@@ -1,28 +1,33 @@
-'use client'
+"use client";
 
-import { UserButton } from '@clerk/nextjs'
-import UnSafePage from '../dob/dobPrompt'
-import { useState } from 'react'
-
+import { UserButton } from "@clerk/nextjs";
+import UnSafePage from "../dob/dobPrompt";
 
 const DotIcon = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+      fill="currentColor"
+    >
       <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z" />
     </svg>
-  )
-}
+  );
+};
 
 export const CustomUserButton = () => {
-  const [birthday, setBirthday] = useState('');
   return (
     <UserButton>
-      <UserButton.UserProfilePage label="Birthday" url="custom" labelIcon={<DotIcon />}>
+      <UserButton.UserProfilePage
+        label="Birthday"
+        url="custom"
+        labelIcon={<DotIcon />}
+      >
         <div>
-          <UnSafePage onBirthdayUpdate={setBirthday} />
+          <UnSafePage />
         </div>
       </UserButton.UserProfilePage>
       <UserButton.UserProfilePage label="account" />
     </UserButton>
-  )
-}
+  );
+};

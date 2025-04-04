1229df2592d7887526e76c432a9c89461c7bb705
Cyang8980 <78817877+Cyang8980@users.noreply.github.com>
2025-04-04 08:00:47 +0800

    fixed config and import in sidebar

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": __dirname,
+      "@/Components": `${__dirname}/Components`,
+      "@/pages": `${__dirname}/pages`,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;

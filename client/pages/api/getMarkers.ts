56d5a530bab7d790af154f261f604c3dd91f3362
Cyang8980 <78817877+Cyang8980@users.noreply.github.com>
2025-04-21 22:44:50 -0400

    any fix in getMarkers.ts

--- client/pages/api/getMarkers.ts
+++ client/pages/api/getMarkers.ts
@@ -1,11 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
+import { Marker } from './insertMarker';
 
 export const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!, 
     process.env.NEXT_PUBLIC_SUPABASE_API_KEY!
 );
 
-export async function getMarkersCurrUserAnon(user_id: string): Promise<any[] | null> {
+export async function getMarkersCurrUserAnon(user_id: string): Promise<Marker[] | null> {
     const { data: markers, error } = await supabase
         .from('markers')
         .select('*')

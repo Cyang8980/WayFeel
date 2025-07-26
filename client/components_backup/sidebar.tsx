3d6435201d9af6ffa8f98a8e940344fee0642c69
Cyang8980 <78817877+Cyang8980@users.noreply.github.com>
2025-07-25 20:26:36 -0400

    ui going crazy

--- client/components_backup/sidebar.tsx
+++ client/components_backup/sidebar.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
 import { useUser, SignInButton } from "@clerk/nextjs";
 import { CustomUserButton } from "@/stuff/CustomUserButton";
-import styled from "styled-stuff";
+import styled from "styled-components";
 import Image from "next/image";
 
 interface SidebarProps {

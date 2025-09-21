86b73f78f368a983a889ac1a77dac45ca8814807
safwan <skadeny@gmail.com>
2025-09-21 15:00:39 -0400

    Dockerfile additions

--- client/next.config.js
+++ client/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_MAP_API_KEY: process.env.NEXT_PUBLIC_MAP_API_KEY,
